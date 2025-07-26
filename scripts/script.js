34bce06da5eb9d5e05a3f93736ad096d429a35a3
Ansab Naif <ansab20556@gmail.com>
2025-07-26 17:50:55 +0300

    add feature of light and dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,28 @@
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('open');
     });
+
+    const toggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark');
+        body.classList.remove('light');
+    } else {
+        body.classList.add('light');
+    }
+
+
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark');
+        body.classList.toggle('light');
+
+        
+        if (body.classList.contains('dark')) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+        }
+    });
+
+    
