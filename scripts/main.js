61c864702d75afbf95fb502b7352b9ba1e1ac48e
Ansab Naif <ansab20556@gmail.com>
2025-07-26 17:39:48 +0300

    edite header and footer of index page and make it responsive

--- scripts/main.js
+++ scripts/main.js
@@ -114,6 +114,28 @@ window.onload = loadStoredData();
 
 
 // dark and light mode
+// const toggleButton = document.getElementById('toggle-button');
+// const body = document.body;
+
+// if (localStorage.getItem('theme') === 'dark') {
+//     body.classList.add('dark');
+//     body.classList.remove('light');
+// } else {
+//     body.classList.add('light');
+// }
+
+
+// toggleButton.addEventListener('click', () => {
+//     body.classList.toggle('dark');
+//     body.classList.toggle('light');
+
+    
+//     if (body.classList.contains('dark')) {
+//         localStorage.setItem('theme', 'dark');
+//     } else {
+//         localStorage.setItem('theme', 'light');
+//     }
+// });
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme') || 'light';
@@ -161,4 +183,42 @@ function filterRows(rows, filter) {
             rows[i].style.display = textValue.includes(filter) ? '' : 'none';
         }
     }
+}
+
+// ------------------------------------------------------
+function validateForm() {
+    let isValid = true;
+
+    // إعادة تعيين الأخطاء
+    document.getElementById("nameError").innerText = "";
+    document.getElementById("emailError").innerText = "";
+    document.getElementById("messageError").innerText = "";
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    // التحقق من الاسم
+    if (name === "") {
+    document.getElementById("nameError").innerText = "الرجاء إدخال الاسم";
+    isValid = false;
+    }
+
+    // التحقق من البريد الإلكتروني
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (email === "") {
+    document.getElementById("emailError").innerText = "الرجاء إدخال البريد الإلكتروني";
+    isValid = false;
+    } else if (!emailPattern.test(email)) {
+    document.getElementById("emailError").innerText = "البريد الإلكتروني غير صالح";
+    isValid = false;
+    }
+
+    // التحقق من الرسالة
+    if (message === "") {
+    document.getElementById("messageError").innerText = "الرجاء إدخال الرسالة";
+    isValid = false;
+    }
+
+    return isValid;
 }
